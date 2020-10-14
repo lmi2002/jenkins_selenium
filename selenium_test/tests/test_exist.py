d359c300e8b7b07b14f147dc5c6fea5c27cc707b
asus <creman2002@gmail.com>
2020-10-14 22:47:53 +0300

    edit Dockerfile

--- selenium_test/tests/test_exist.py
+++ selenium_test/tests/test_exist.py
@@ -8,6 +8,6 @@ class TestExample(SettingBrowser):
     @pytest.mark.smoke
     def test_go_to_exist(self):
         driver = self.get_driver()
-        driver.maximize_window()
+        driver.fullscreen_window()
         driver.get('https://exist.ua/')
         driver.close()
